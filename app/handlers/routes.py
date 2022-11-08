0f654c427163d97ad77f0277929a91838f6d1094
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2022-11-08 05:50:57 -0500

    added validation and test cases for it

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -5,32 +5,37 @@ import pandas as pd
 import numpy as np
 import os
 
-
 def configure_routes(app):
-
+    # load model from 'database'
     this_dir = os.path.dirname(__file__)
     model_path = os.path.join(this_dir, "model.pkl")
     clf = joblib.load(model_path)
 
+    # default
     @app.route('/')
     def hello():
         return "try the predict route it is great!"
 
-
+    # predict a student
     @app.route('/predict')
     def predict():
-        # Planned modifications for next part of Hw4: 
-        #   - change this fucntion to take json of one or many students and give back one or more G3 scores
-        #   - use entries from the query string here but could also use json
-        # Below function only implemented in case of prediction on one student
-        # instead of multiple. Multiple student implementation will hold in 
-        # next homework. 
+        # we use the G1, G2, absences, studytime, and failures features
+        # data sent in parameters of request
         g1 = int(request.args.get('G1'))
-        absences = int(request.args.get('absences'))
         g2 = int(request.args.get('G2'))
         studytime = int(request.args.get('studytime'))
         failures = int(request.args.get('failures'))
-        data = [[g1], [g2], [absences],[studytime],[failures]]
+        absences = int(request.args.get('absences'))
+        #data = [[g1], [g2], [absences],[studytime],[failures]]
+        
+        # validate data
+        assert(g1 in range(0, 20)) # (numeric: from 0 to 20)
+        assert(g2 in range(0, 20))# (numeric: from 0 to 20)
+        assert(studytime in range(1, 4)) # (numeric: 1 - <2 hours, 2 - 2 to 5 hours, 3 - 5 to 10 hours, or 4 - >10 hours)
+        assert(failures in range(1, 4)) # (numeric: n if 1<=n<3, else 4)
+        assert(absences in range(0, 93)) # (numeric: from 0 to 93)
+        
+        # load data into query_df
         query_df = pd.DataFrame({
             'G1': pd.Series(g1),
             'G2': pd.Series(g2),
@@ -38,7 +43,10 @@ def configure_routes(app):
             'studytime': pd.Series(studytime),
             'failures': pd.Series(failures)
         })
+        
+        # set query and run predict ML
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
+        
         return jsonify(np.ndarray.item(prediction))
     
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -22,9 +22,23 @@ def test_predict_route():
     client = app.test_client()
     url = '/predict'
 
+    # missing data test
     missing_data={'Dalc':1, 'failures':3, 'G1':18, 'G2':20, 'absences':4}
     response1 = client.get(url, query_string=missing_data)
     assert response1.status_code == 500
+    
+    # invalid data test
+    invalid_data_queries = [
+        {'G1': 30, 'G2': 8, 'studytime': 2, 'failures': 4, 'absences': 0}, # G1
+        {'G1': 20, 'G2': -2, 'studytime': 2, 'failures': 4, 'absences': 0}, # G2
+        {'G1': 20, 'G2': 8, 'studytime': 10, 'failures': 4, 'absences': 0}, # studytime
+        {'G1': 20, 'G2': 8, 'studytime': 2, 'failures': -10, 'absences': 0}, # failres
+        {'G1': 20, 'G2': 8, 'studytime': 2, 'failures': 4, 'absences': 94}] # absences
+    for query in invalid_data_queries:
+        invalid = client.get(url, query_string=query)
+        assert invalid.status_code == 500
+        
+        
     #assert response1.get_data() == b'Error: Not given one or more required input columns.'
 
     # incorrect_data={'studytime':1, 'failures':-2, 'G1':18, 'G2':20, 'absences':4}
@@ -32,6 +46,7 @@ def test_predict_route():
     # assert response2.status_code == 500
     #assert response2.get_data() == b'Error: One of the column values are invalid.'
 
+    # success test
     correct_data={'studytime':2, 'failures':0, 'G1':19, 'G2':19, 'absences':0}
     response3 = client.get(url, query_string=correct_data)
     assert response3.status_code == 200
